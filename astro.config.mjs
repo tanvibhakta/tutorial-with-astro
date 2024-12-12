f219c9156c421e64f63db0b9909b0d78b4c8196b
Tanvi Bhakta <tanvibhakta@gmail.com>
2024-12-12 16:43:24 +0530

    use preact in the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tanvi-astro-tutorial.netlify.app/"
+  site: "https://tanvi-astro-tutorial.netlify.app/",
+  integrations: [preact()]
 });
